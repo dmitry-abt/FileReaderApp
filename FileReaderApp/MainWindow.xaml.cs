6498ec44e0aabb97a4c7fb30962c45efbeece7bd
Dmitry Abt <dmitry.abt@gmail.com>
2025-03-19 07:59:12 +0300

    added a draft of the project

---
+++ FileReaderApp/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace FileReaderApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileContent = File.ReadAllText(filePath);
+                FileContentTextBox.Text = fileContent;
+            }
+        }
+    }
+}
